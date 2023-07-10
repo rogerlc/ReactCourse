3d36767ff3e92b320266a6879911eefa36ae2c29
Roger Luna <rogeilesox@gmail.com>
2023-07-10 13:04:32 -0600

    difference way to combine the arrays

--- main.js
+++ main.js
@@ -1,52 +1,49 @@
-const school = {
-    id :3345,
-    type: 'swimming',
-    levelsDescrp: '[beginner, intermediate, advanced]',
-    nameSchool: 'Swimming Cool',
-    inscription:500,
-    pageMounth: 900,    
-    Itemlevels:[
-        {
-            type:'begginer',
-            pageInscr: 300,
-            pageMounth: 500,
-            pageYear: 0,
-            quality:1,
-        },
-        {
-            type:'Intermediate',
-            pageInscr: 500,
-            pageMounth: 700,
-            pageYear: 0,
-            quality:1,
-        },
-        {
-            type:'Advanced',
-            pageInscr: 600,
-            pageMounth: 900,
-            pageYear: 0,
-            quality:1,
-            
-        },
-    ],
-    promoQuality:function(){
-        let total = 0;
-        this.Itemlevels.forEach(item =>{
-            total = total + item.pageInscr*item.quality
-        });
-        return total;
+/*
+// se utiliza el operador spread que es el ...
+//Ejemplo de añadir datos de un arreglo en otro arreglo y al final utilizar push
+const products=['mesa','silla','teclado'];
+const fruits=['peras','manzanas','sandias','frutillas'];
+const mercado=[...fruits,'lechuga','papas','uvas'];
+ mercado.push('mango');
 
-    },
-    firstPageF:function(){
-        const firstpage=`El pago inicial es de: ${this.inscription+this.pageMounth}`;
-        return firstpage;
-    },
-    client:{
-        id:3324,
-        nameClient:'Roge',
-        age: 34
-    }
-}
-//let namesS= school.nameSchool;
-//console.log( school.firstPageF());
-console.log('Total:' + school.promoQuality());
+console.log(products);
+console.log(fruits);
+console.log(mercado);
+*/
+/*
+//Ejemplo de convinar 2 arreglos en uno solo
+const products=['mesa','silla','teclado'];
+const fruits=['peras','manzanas','sandias','frutillas'];
+const mercado=[...fruits,...products]; 
+
+console.log(products);
+console.log(fruits);
+console.log(mercado);
+*/
+
+/*
+//Ejemplo de convinar 2 arreglos mas dos datos mas
+const products=['mesa','silla','teclado'];
+const fruits=['peras','manzanas','sandias','frutillas'];
+const mercado=[...fruits,...products,'papas','uvas']; 
+
+console.log(products);
+console.log(fruits);
+console.log(mercado);
+*/
+
+//Ejemplo de concatenar con concat
+const products=['mesa','silla','teclado'];
+const product2= products.concat(['notepad'])
+const fruits=['peras','manzanas','sandias','frutillas'];
+const mercado=[...fruits,'papas','uvas',...products,...product2]; 
+const mercado2=[product2.concat(fruits).concat('lechuga','papas','uvas')]; 
+
+console.log(product2);
+console.log(mercado);
+console.log(mercado2);
+
+
+
+
+
